474f012d600ce524f46d43e68c19abc2526513c0
Disk Poppy <diskpoppy@protonmail.com>
2022-09-20 22:58:27 +0200

    color math water

--- uberasm/level/cityofgulpgulp.asm
+++ uberasm/level/cityofgulpgulp.asm
@@ -1,12 +1,46 @@
 !waterTimer = $1864|!addr  ;free RAM, must be shared with the water activator sprite
 
-;init:
+init:
 ;STZ !waterTimer  ;only in case you change free RAM to one not reset on level load
-;RTL
+
+STZ $2121
+LDA $0701|!addr  ;bg color
+STA $2122
+LDA $0702|!addr  ;bg color
+STA $2122
+
+LDA #%10110101
+STA $40  ;CGADSUB mirror
+
+LDA #%00010101
+STA $212C  ;TM
+STA $212E  ;TMW
+LDA #%00000010
+STA $212D  ;TS
+STA $212F  ;TSW
+
+RTL
+
 
 main:
+LDA #%00010101
+STA $212C  ;TM
+STA $212E  ;TMW
+LDA #%00000010
+STA $212D  ;TS
+STA $212F  ;TSW
+
+LDA $9D  ;lock sprites
+ORA $13D4|!addr  ;pause
+BNE .dontdec
+
 LDA !waterTimer
-BEQ +
+BEQ .dry
+.wet
+LDA #$64
+STA $0701|!addr  ;bg color
+STZ $0702|!addr  ;bg color
+
 LDA #$01
 STA $85  ;water level flag
 LDA $14
@@ -15,6 +49,10 @@ BNE .dontdec
 DEC !waterTimer
 .dontdec
 RTL
-+
+
+.dry
+STZ $0701|!addr  ;bg color
+STZ $0702|!addr  ;bg color
+
 STZ $85  ;water level flag
 RTL
