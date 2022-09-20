e2167fe22b04839ecf8252a10393902371f06195
Disk Poppy <diskpoppy@protonmail.com>
2022-09-20 23:22:10 +0200

    smooth transition

--- uberasm/level/cityofgulpgulp.asm
+++ uberasm/level/cityofgulpgulp.asm
@@ -11,14 +11,6 @@ STA $2122
 
 LDA #%10110101
 STA $40  ;CGADSUB mirror
-
-LDA #%00010101
-STA $212C  ;TM
-STA $212E  ;TMW
-LDA #%00000010
-STA $212D  ;TS
-STA $212F  ;TSW
-
 RTL
 
 
@@ -34,10 +26,16 @@ LDA $9D  ;lock sprites
 ORA $13D4|!addr  ;pause
 BNE .dontdec
 
-LDA !waterTimer
+LDX !waterTimer
 BEQ .dry
 .wet
+CPX #$0F
+BCC +
 LDA #$64
+BRA ++
++
+LDA bgColor,x
+++
 STA $0701|!addr  ;bg color
 STZ $0702|!addr  ;bg color
 
@@ -56,3 +54,7 @@ STZ $0702|!addr  ;bg color
 
 STZ $85  ;water level flag
 RTL
+
+bgColor:
+db $00,$20,$20,$22,$22,$42,$42,$43
+db $43,$43,$63,$63,$63,$63,$63,$64
